62f8e1c8050688215db1195a05715018a1ee1ef3
Ndumiso Blose <blosendumo@gmail.com>
2024-06-25 13:46:01 -0700

    print the sign of the number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of the number
+ * @n: chacter chosen to be checked
+ *
+ * Return: 1 prints + if n is greater than 0, 0 prints 0 if is 0, -1 prints - if <0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar("+");
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar("-");
+		return (-1);
+	}
+	else
+	{
+		_putchar("0");
+		return (0);
+	}
+}
