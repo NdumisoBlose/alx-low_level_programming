8680856163a539e86e1173579094cb7f47cbac4b
Ndumiso Blose <blosendumo@gmail.com>
2024-06-25 15:33:56 -0700

    prints 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int a, b, c, v, z;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+
+			if (c < 9)
+			{
+				v = c % 10;
+				z = (c - v) / 10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(z + '0');
+				_putchar(v + '0');
+			}
+			else
+			{
+				if (b != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(c + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
