f88f5b2b293af7b197f426c609ad2e4b7d0f6028
Ndumiso Blose <blosendumo@gmail.com>
2024-06-27 11:52:25 -0700

    draws a straight line in the terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight in terminal
+ * @n: value for the length of the line
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
