1f1e8fedb1c68f265d2f035679acb9181677d45c
Ndumiso Blose <blosendumo@gmail.com>
2024-07-01 11:23:32 -0700

    redirecting an int parameter to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates to 98
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	int i = 98;
+
+	int *n = &i;
+}
